19b951d44c659073c205fbc503e92b1ee008e429
YaBoiSkinnyP <jadenrodriguez7@gmail.com>
2023-06-06 04:23:16 -0500

    added notesheets

--- src/decryptogame/game.py
+++ src/decryptogame/game.py
@@ -1,12 +1,29 @@
 import dataclasses
 from collections.abc import Sequence
 
+@dataclasses.dataclass(kw_only=True)
+class Note:
+    clues: Sequence[str] = None
+    attempted_decipher: Sequence[int] = None
+    attempted_interception: Sequence[int] = None
+    correct_code: Sequence[int] = None
+
 
 @dataclasses.dataclass(kw_only=True)
 class Game:
-    words: Sequence[Sequence[str]]
-    winner: int = None # right now, player are represented as integers. It might make sense to use Enum later.
-    rounds_played: int = 0
-    miscommunications: list[int] = dataclasses.field(default_factory=lambda:[0,0])
-    interceptions: list[int] = dataclasses.field(default_factory=lambda:[0,0])
+    keywords: Sequence[Sequence[str]]
+    notesheets: Sequence[list[Note]] = dataclasses.field(default_factory=lambda: [[], []])
+     
+    def __post_init__(self):
+        self.rounds_played = len(self.notesheets[0])
+        self.miscommunications = [0, 0]
+        self.interceptions = [0, 0]
+
+        for team, notesheet in enumerate(self.notesheets): # may make sense to abstract this to separate method later
+            opponent = not team
+            for note in notesheet:
+                if note.attempted_decipher != note.correct_code:
+                    self.miscommunications[team] += 1
+                if note.attempted_interception == note.correct_code:
+                    self.interceptions[opponent] += 1
 
--- tests/test_game.py
+++ tests/test_game.py
@@ -1,37 +1,69 @@
 import pytest
-from decryptogame.game import Game
+from decryptogame.game import Game, Note
+
+EXAMPLE_KEYWORDS = (
+    ("black", "dragonfly", "cocktail", "sombrero"),
+    ("antiquity", "bone", "morning", "nightmare")
+)
+
+@pytest.fixture()
+def default_game():
+    return Game(keywords=EXAMPLE_KEYWORDS)
 
 class TestGame:
-    def test_default(self):
-        words = [
-            ("black", "dragonfly", "cocktail", "sombrero"),
-            ("antiquity", "bone", "morning", "nightmare")
-        ]
-        game = Game(words=words)
+    def test_default(self, default_game):
+        assert default_game.keywords == EXAMPLE_KEYWORDS
 
-        assert game.words == words
+        assert default_game.rounds_played == 0
 
-        assert game.miscommunications[0] == 0
-        assert game.miscommunications[1] == 0
+        assert not default_game.notesheets[0]
+        assert not default_game.notesheets[1]
 
-        assert game.interceptions[0] == 0
-        assert game.interceptions[1] == 0
+        assert default_game.miscommunications[0] == 0
+        assert default_game.miscommunications[1] == 0
 
-        assert game.winner is None
-        assert game.rounds_played == 0        
-
+        assert default_game.interceptions[0] == 0
+        assert default_game.interceptions[1] == 0
 
     def test_kw_only(self):
         with pytest.raises(TypeError):
             game = Game(0)
     
     def test_configurable(self):
-        words = [
-            ("black", "dragonfly", "cocktail", "sombrero"),
-            ("antiquity", "bone", "morning", "nightmare")
+        # one miscommunication, one opponent interception
+        first_notesheet = [ 
+            Note(attempted_interception=(2, 1, 3), attempted_decipher=(1, 2, 3), correct_code=(1, 2, 3)),
+            Note(attempted_interception=(2, 1, 3), attempted_decipher=(2, 1, 3), correct_code=(2, 1, 3)), # interception
+            Note(attempted_interception=(2, 3, 1), attempted_decipher=(2, 3, 1), correct_code=(4, 3, 1))  # miscommunication
         ]
-        game = Game(words=words, miscommunications=[1,2], interceptions=[3,4])
+        # zero miscommunications, one opponent interceptions
+        second_notesheet = [
+            Note(attempted_interception=(4, 1, 3), attempted_decipher=(3, 1, 4), correct_code=(3, 1, 4)),
+            Note(attempted_interception=(2, 4, 3), attempted_decipher=(4, 1, 3), correct_code=(4, 1, 3)),
+            Note(attempted_interception=(2, 1, 3), attempted_decipher=(2, 1, 3), correct_code=(2, 1, 3)) # interception
+        ]
+        assert len(first_notesheet) == len(second_notesheet) # this is to ensure test was made properly
+        rounds_played = len(first_notesheet)
+        notesheets = [
+            first_notesheet,
+            second_notesheet
+        ]
+        game = Game(keywords=EXAMPLE_KEYWORDS, notesheets=notesheets)
+        assert game.rounds_played == rounds_played
         assert game.miscommunications[0] == 1
-        assert game.miscommunications[1] == 2
-        assert game.interceptions[0] == 3
-        assert game.interceptions[1] == 4
+        assert game.miscommunications[1] == 0
+        assert game.interceptions[0] == 1
+        assert game.interceptions[1] == 1
+
+class TestNote:
+    # each team will have a notesheet, a sequence of notes
+    def test_default(self):
+        note = Note()
+        assert note.clues is None
+        assert note.attempted_decipher is None
+        assert note.attempted_interception is None
+        assert note.correct_code is None
+
+    def test_kw_only(self):
+        with pytest.raises(TypeError):
+            note = Note(0)
