954b014109fc9e6ea53d655a62a08c08fdfb57a3
YaBoiSkinnyP <jadenrodriguez7@gmail.com>
2023-07-29 00:33:13 -0500

    improved interfaces

--- src/decryptogame/teams.py
+++ src/decryptogame/teams.py
@@ -1,29 +1,26 @@
-import abc
+from typing import Protocol
 import dataclasses
 import random
 from decryptogame.components import Code, Clue, TeamName
 from decryptogame.game import Game
 
-class Encryptor(abc.ABC):
+class Encryptor(Protocol):
     """Interface representing an Encryptor, a teammate who decides clues"""
 
-    @abc.abstractmethod
     def decide_clues(self, team_name: TeamName, game: Game, code: Code) -> Clue:
-        pass
+        ...
 
-class Intercepter(abc.ABC):
+class Intercepter(Protocol):
     """Interface representing an Intercepter, a teammate who attempts to decipher the opposing team's clues"""
 
-    @abc.abstractmethod
     def intercept_clues(self, team_name: TeamName, game: Game, opponent_clues: Clue) -> Code:
-        pass
+        ...
 
-class Guesser(abc.ABC):
+class Guesser(Protocol):
     """Interface representing an Guesser, a teammate who attempts to decipher their team's clues"""
 
-    @abc.abstractmethod
     def decipher_clues(self, team_name: TeamName, game: Game, clues: Clue) -> Code:
-        pass
+        ...
     
 @dataclasses.dataclass(kw_only=True)
 class Team:
